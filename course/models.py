88ca260859f08a775b654205da06ee7ffe287f7f
ujeongoh <yujoh94@gmail.com>
2023-05-01 20:20:50 +0900

    feat : 모델 생성 및 마이그레이션

--- course/models.py
+++ course/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    instructor = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    rating = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True)
+    thumbnail_url = models.URLField(max_length=200, null=True, blank=True)
+    is_package = models.BooleanField(default=False)
+    is_free = models.BooleanField(default=False)
+    enrollment_count = models.IntegerField(default=0)
+    url = models.URLField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=False)
+    tags = models.ManyToManyField('Tag')
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey('Course', on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'course')
+
+    def __str__(self):
+        return f'{self.user.username} - {self.course.title}'
