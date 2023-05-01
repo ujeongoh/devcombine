6e40a0ddc8a0f6b17bf0b11349522e60e9c45e2a
ujeongoh <yujoh94@gmail.com>
2023-05-01 21:19:52 +0900

    fix : 모델 구조 변경

--- course/models.py
+++ course/models.py
@@ -3,33 +3,33 @@ from django.contrib.auth.models import User
 
 
 class Course(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=300)
     instructor = models.CharField(max_length=200)
     description = models.TextField()
+    url = models.URLField(max_length=500)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     rating = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True)
-    thumbnail_url = models.URLField(max_length=200, null=True, blank=True)
+    thumbnail_url = models.URLField(max_length=500, null=True, blank=True)
     is_package = models.BooleanField(default=False)
     is_free = models.BooleanField(default=False)
     enrollment_count = models.IntegerField(default=0)
-    url = models.URLField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=False)
-    tags = models.ManyToManyField('Tag')
 
     def __str__(self):
         return self.title
 
 
 class Tag(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
 
-class Wishlist(models.Model):
+class UserCourse(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    course = models.ForeignKey('Course', on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
 
     class Meta:
         unique_together = ('user', 'course')
