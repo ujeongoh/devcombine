ebddb44059fd219aa4cebe0fea83835c85424f89
sejeong-park <tpwjd29@gmail.com>
2023-05-07 02:45:55 +0900

    fix : Class 기반 View

--- series/urls.py
+++ series/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'series'
 
 urlpatterns = [
-    path('', views.main_series, name='main_series'),
+    path('', views.SeriesListView.as_view(), name='main_series'),
+    path('list/', views.SeriesListView.as_view(), name='total_seires'),
     path('<int:series_id>/', views.SereisCourseListView.as_view(), name='series_course')
 ]
--- series/views.py
+++ series/views.py
@@ -1,4 +1,3 @@
-from django.forms import model_to_dict
 from django.shortcuts import render
 from django.db.models import Count
 from django.views.generic import ListView
@@ -8,16 +7,25 @@ from django.db.models.functions import Random
 from courses.models import Course
 from series.models import Series
 
-# class SeriesListView(ListView):
-#     """
-#     SeriesListView
-#     - 전체 시리즈
-#     """
-#     model = Series
-#     context_object_name = 'series_list'
-#     template_name = 'series/index.html'
 
-#     # /series/list/ 라고 치면 Total series가 나와야 함
+class SeriesListView(ListView):
+    """
+    SeriesListView
+    - 전체 시리즈
+    """
+    model = Series
+    template_name = 'series/index.html'
+    context_object_name = 'main_series'
+
+    def get_queryset(self):
+        """
+        메인에 노출 시키는 시리즈의 목록만 가져오는 함수
+        - Admin에서 Sries를 지정할 때 is_main = True 로 노출 시킨 값만 필터링 한다.
+        """
+        main_series = Series.objects.filter(is_main=True).order_by(Random())[
+            :3]        # main화면에 3개만 Random
+
+        return main_series
 
 
 class SereisCourseListView(ListView):
@@ -38,18 +46,28 @@ class SereisCourseListView(ListView):
             'series': series,
             'matching_courses': matching_courses,
         }
-        
+
         # TODO :: context 에 Pagenator 설정 필요
 
         return render(request, 'series/series_course.html', context)
 
 
-def main_series(request):
-    """
-    메인에 노출 시키는 시리즈의 목록만 가져오는 함수
-    - Admin에서 Sries를 지정할 때 is_main = True 로 노출 시킨 값만 필터링 한다.
-    """
-    main_series = Series.objects.filter(is_main=True).order_by(Random())[
-        :3]        # main화면에 3개만 Random
+# class SeriesListView(ListView):
+#     """
+#     SeriesListView
+#     - 전체 시리즈
+#     """
+#     model = Series
+#     template_name = 'series/index.html'
+#     context_object_name = 'main_seiries'
+#     def get(self, request):
+#         """
+#         메인에 노출 시키는 시리즈의 목록만 가져오는 함수
+#         - Admin에서 Sries를 지정할 때 is_main = True 로 노출 시킨 값만 필터링 한다.
+#         """
+#         main_series = Series.objects.filter(is_main=True).order_by(Random())[
+#             :3]        # main화면에 3개만 Random
 
-    return render(request, 'series/index.html', {'main_series' : main_series})
+#         return render(request, 'series/index.html', {'main_series': main_series})
+
+#     # /series/list/ 라고 치면 Total series가 나와야 함
