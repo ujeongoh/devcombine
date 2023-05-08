ce31f5c4dcb6db76fcc1ecdc71b76e8d8276292d
ujeongoh <yujoh94@gmail.com>
2023-05-09 01:18:22 +0900

    fix : allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['18.191.102.241']
+ALLOWED_HOSTS = ['.us-east-2.compute.amazonaws.com']
 
 
 # Application definition
